702cf3fee0b97b9f841a81cdf33aa1a82bf0cb05
Yog <yog.panjarale@gmail.com>
2021-12-27 00:37:11 +0530

    todos added

--- src/lib/imagecard.svelte
+++ src/lib/imagecard.svelte
@@ -1,6 +1,8 @@
 <script>
     export let photo;
-
+    //TODO: downdload button
+    //TODO: Copy code button
+    //TODO: link to source button
 </script>
 
 <img  class= "h-72 shadow-2xl" src ={photo.urls.regular} alt = {photo.descrption} />
